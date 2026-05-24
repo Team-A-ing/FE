6830a09687e23c61944a11fd88f1b899a66b8ca4
ilwhs31 <ilwhs31@gmail.com>
2026-05-24 11:02:55 +0900

    #FEAT: 멤버용 1on1 미팅 페이지 생성 및 사이드 바 수정 (1on1 만들기 버튼 삭제)

---
+++ src/pages/member/MeetingsPage.tsx
@@ -0,0 +1,11 @@
+import { ROUTES } from "@/constants/routes";
+import LeaderMeetingsPage from "@/pages/leader/MeetingsPage";
+
+export default function MemberMeetingsPage() {
+  return (
+    <LeaderMeetingsPage
+      showCreateButton={false}
+      getMeetingPath={ROUTES.MEMBER_MEETING}
+    />
+  );
+}
